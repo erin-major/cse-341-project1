2de2d4d563d995b86d7b3c7a42e0d5e17772a373
erin.major <erinmajor254@gmail.com>
2026-03-07 15:33:58 -0700

    Connect to mongodb

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const mongodb = require('./database/connect');
+const contactsRoutes = require('./routes/contacts');
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
 });
+app.use('/', require('./routes'));
+app.use('/contacts', contactsRoutes);
 
-app.listen(port, () => {  console.log(`Example app listening at http://localhost:${port}`);
+mongodb.initDb((err, mongodb) => {
+    if (err) {
+        console.error(err);
+    } else {
+        app.listen(port);
+        console.log(`Connected to DB and listening on port ${port}`);
+    }
 });
