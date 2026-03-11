c9ba39c87b1dd77f95e2befa7efbfc72d3a3b366
erin.major <erinmajor254@gmail.com>
2026-03-10 21:17:52 -0600

    Install prettier and prettier things

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -2,26 +2,26 @@ const mongodb = require('../database/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 /* ****************************************
-*  Deliver all contacts data
-* *************************************** */
-async function getAllContacts(req, res, next) {
-    const results = await mongodb.getDb().db().collection('contacts').find();
-    results.toArray().then((contacts) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(contacts);
-    });
+ *  Deliver all contacts data
+ * *************************************** */
+async function getAllContacts(req, res) {
+  const results = await mongodb.getDb().db().collection('contacts').find();
+  results.toArray().then((contacts) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(contacts);
+  });
 }
 
 /* ****************************************
-*  Deliver contact data by id
-* *************************************** */
-async function getContactById(req, res, next) {
-    const contactId = new ObjectId(req.params.id);
-    const results = await mongodb.getDb().db().collection('contacts').find({ _id: contactId });
-    results.toArray().then((contacts) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(contacts[0]);
-    });
+ *  Deliver contact data by id
+ * *************************************** */
+async function getContactById(req, res) {
+  const contactId = new ObjectId(req.params.id);
+  const results = await mongodb.getDb().db().collection('contacts').find({ _id: contactId });
+  results.toArray().then((contacts) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(contacts[0]);
+  });
 }
 
 module.exports = { getAllContacts, getContactById };
--- server.js
+++ server.js
@@ -1,24 +1,23 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
 const mongodb = require('./database/connect');
 const contactsRoutes = require('./routes/contacts');
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
 });
 app.use('/', require('./routes'));
 app.use('/contacts', contactsRoutes);
 
-mongodb.initDb((err, mongodb) => {
-    if (err) {
-        console.error(err);
-    } else {
-        app.listen(port);
-        console.log(`Connected to DB and listening on port ${port}`);
-    }
+mongodb.initDb((err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on port ${port}`);
+  }
 });
