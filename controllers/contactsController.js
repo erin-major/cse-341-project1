404c77c9d4ffb8d30eccac40a001176c8368a4f6
erin.major <erinmajor254@gmail.com>
2026-03-13 20:07:05 -0600

    Finish contacts APIs

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -24,4 +24,77 @@ async function getContactById(req, res) {
   });
 }
 
-module.exports = { getAllContacts, getContactById };
+/* ****************************************
+ *  Add a contact
+ * *************************************** */
+async function createContact(req, res) {
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const result = await mongodb.getDb().db().collection('contacts').insertOne(contact);
+  if (result.acknowledged) {
+    res.status(201).json(result.insertedId);
+  } else {
+    res.status(500).json(result.error || 'An error occurred while creating the contact.');
+  }
+}
+
+/* ****************************************
+ *  Update a contact
+ * *************************************** */
+async function updateContact(req, res) {
+  const contactId = new ObjectId(req.params.id);
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const result = await mongodb
+    .getDb()
+    .db()
+    .collection('contacts')
+    .updateOne(
+      { _id: contactId },
+      {
+        $set: {
+          firstName: contact.firstName,
+          lastName: contact.lastName,
+          email: contact.email,
+          favoriteColor: contact.favoriteColor,
+          birthday: contact.birthday
+        }
+      }
+    );
+  if (result.modifiedCount === 1) {
+    res.status(200).send();
+  } else {
+    res.status(500).json(result.error || 'An error occurred while updating the contact.');
+  }
+}
+
+/* ****************************************
+ *  Delete contact data by id
+ * *************************************** */
+async function deleteContactById(req, res) {
+  const contactId = new ObjectId(req.params.id);
+  const result = await mongodb.getDb().db().collection('contacts').deleteOne({ _id: contactId });
+  if (result.deletedCount === 1) {
+    res.status(200).send();
+  } else {
+    res.status(500).json(result.error || 'An error occurred while deleting the contact.');
+  }
+}
+
+module.exports = {
+  getAllContacts,
+  getContactById,
+  createContact,
+  updateContact,
+  deleteContactById
+};
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
+  // #swagger.summary = 'Welcome message'
+  // #swagger.description = 'Endpoint to return a welcome message.'
+  next();
   res.send('Hello World!');
 });
 
