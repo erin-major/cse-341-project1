bf08b0c205fd52fab589f21534a751a35746dd79
erin.major <erinmajor254@gmail.com>
2026-03-18 18:55:30 -0600

    validation

---
+++ middleware/contact-validation.js
@@ -0,0 +1,26 @@
+const validate = require('../helpers/validate');
+
+const saveContact = (req, res, next) => {
+  const validationRules = {
+    firstName: 'required|string',
+    lastName: 'required|string',
+    email: 'required|email',
+    favoriteColor: 'required|string',
+    birthday: 'required|string'
+  };
+  validate(req.body, validationRules, {}, (err, status) => {
+    if (!status) {
+      res.status(412).send({
+        success: false,
+        message: 'Validation failed',
+        data: err
+      });
+    } else {
+      next();
+    }
+  });
+};
+
+module.exports = {
+  saveContact
+};
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const contactsController = require('../controllers/contactsController');
+const contactValidation = require('../middleware/contact-validation');
 
 router.get('/', contactsController.getAllContacts);
 router.get('/:id', contactsController.getContactById);
-router.post('/', contactsController.createContact);
-router.put('/:id', contactsController.updateContact);
+router.post('/', contactValidation.saveContact, contactsController.createContact);
+router.put('/:id', contactValidation.saveContact, contactsController.updateContact);
 router.delete('/:id', contactsController.deleteContactById);
 
 module.exports = router;
